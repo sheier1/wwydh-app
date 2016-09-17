3609fa949631a0d93694fa4ca6f2daec0ba0bac1
nrwinner <WinnerDigital@laptop-2.local>
2016-09-17 12:47:50 -0400

    maps stuffz

--- home/index.php
+++ home/index.php
@@ -7,9 +7,7 @@
 
     $data = $q->get_result();
 
-    while($row = $data->fetch_array(MYSQLI_ASSOC)) {
-        echo $row["address"];
-    }
+    $row = $data->fetch_array(MYSQLI_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,25 +18,32 @@
                     var lat = location.coords.latitude;
                     var lng = location.coords.longitude;
 
-                    var newLat = "<?php echo $row["latitude"] ?>";
-                    var newLng = "<?php echo $row["longitude"] ?>";
+                    var newLat = parseFloat("<?php echo $row["latitude"] ?>");
+                    var newLng = parseFloat("<?php echo $row["longitude"] ?>");
 
-                    alert(newLat + ", " + newLng);
+                    var newLat1 = 39.363661;
+                    var newLng1 = -76.610457;
 
                     var myLatLng = {lat: lat, lng: lng};
                     var position = {lat: newLat, lng: newLng};
+                    var position2 = {lat: newLat1, lng: newLng1}
 
                     // Create a map object and specify the DOM element for display.
                     var map = new google.maps.Map(document.getElementById('map'), {
-                      center: position,
+                      center: myLatLng,
                       scrollwheel: false,
-                      zoom: 10
+                      zoom: 11
                     });
 
                     var marker = new google.maps.Marker({
                         map: map,
                         position: position
                     });
+
+                    new google.maps.Marker({
+                        map: map,
+                        position: position2
+                    })
                 })
             }
 
