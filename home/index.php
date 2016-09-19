97701e27f40bd76dc81b0a95be25743566e55b64
nrwinner <WinnerDigital@laptop-2.local>
2016-09-19 01:59:34 -0400

    building homepage backend

--- home/index.php
+++ home/index.php
@@ -2,52 +2,49 @@
 
     include "../helpers/conn.php";
 
-    $q = $conn->prepare("SELECT * FROM locations"); // change with where clause (select closest 4-10 locations)
+    // TODO:0 change location select query to count ideas and grab features for each location
+    $q = $conn->prepare("SELECT l.* FROM locations l "); // pulls all locations right now, do we want to keep this?
     $q->execute();
 
     $data = $q->get_result();
+    $locations = [];
 
-    $row = $data->fetch_array(MYSQLI_ASSOC);
+    while ($row = $data->fetch_array(MYSQLI_ASSOC)) array_push($locations, $row);
 ?>
 <!DOCTYPE html>
 <html>
     <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
         <script type="text/javascript">
+            // convert location data from php to javascript using JSON
+            var locations = jQuery.parseJSON('<?php echo str_replace("'", "\'", json_encode($locations)) ?>');
+
+            // TODO:10 implement a way to sort locations array by closest. Thinking a load file that grabs current location (or doesn't) and then calls this page with $_GET vars?
+
             function initMap() {
                 navigator.geolocation.getCurrentPosition(function(location) {
                     var lat = location.coords.latitude;
                     var lng = location.coords.longitude;
 
-                    var newLat = parseFloat("<?php echo $row["latitude"] ?>");
-                    var newLng = parseFloat("<?php echo $row["longitude"] ?>");
-
-                    var newLat1 = 39.363661;
-                    var newLng1 = -76.610457;
-
                     var myLatLng = {lat: lat, lng: lng};
-                    var position = {lat: newLat, lng: newLng};
-                    var position2 = {lat: newLat1, lng: newLng1}
 
                     // Create a map object and specify the DOM element for display.
                     var map = new google.maps.Map(document.getElementById('map'), {
-                      center: myLatLng,
+                      center: {lat: parseFloat(locations[0].latitude), lng: parseFloat(locations[0].longitude)},
                       scrollwheel: false,
-                      zoom: 11
+                      zoom: 13
                     });
 
-                    new google.maps.Marker({
-                        map: map,
-                        position: myLatLng
-                    });
+                    $(locations).each(function() {
+                        var marker = new google.maps.Marker({
+                            map: map,
+                            position: {lat: parseFloat(this.latitude), lng: parseFloat(this.longitude)},
+                            address: this.mailing_address
+                        });
 
-                    new google.maps.Marker({
-                        map: map,
-                        position: position
-                    });
-
-                    new google.maps.Marker({
-                        map: map,
-                        position: position2
+                        marker.addListener("click", function() {
+                            alert(this.address);
+                        })
                     })
                 })
             }
@@ -56,11 +53,32 @@
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBzAMBl8WEWkqExNw16kEk40gCOonhMUmw&callback=initMap" async defer></script>
     </head>
     <body>
-        <div id="nav"></div>
-        <div id="map" style="height: 500px">
-
+        <div id="nav">
+            <div id="logo"></div>
+            <div id="main_nav"></div>
+            <div id="user_nav"></div>
         </div>
-        <div id="explore"></div>
+        <div id="map" style="height: 500px"></div>
+        <div id="explore">
+            <div>
+                <?php
+                foreach($locations as $l) { ?>
+                    <div class="location">
+                        <div class="location_image"></div>
+                        <div class="address"><?php echo $l["address"] ?></div>
+                        <div class="features">
+                            <span>Features:</span>
+                                <ul>
+                                    <?php foreach ($locations["features"] as $f) { ?>
+
+                                    <?php } ?>
+                                </ul>
+                        </div>
+                    </div>
+                <?php }
+                ?>
+            </div>
+        </div>
         <div id="about"></div>
         <div id="how"></div>
         <div id="contact"></div>
