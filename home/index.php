694fda0937318bf4a57f4a9382a916e6e3b18f7f
mjkrause14 <mjkrause14@gmail.com>
2016-10-10 17:42:19 -0400

    Update index.php

--- home/index.php
+++ home/index.php
@@ -1,9 +1,11 @@
 <?php
 
+    session_start();
+
     include "../helpers/conn.php";
 
     // BACKEND:0 change homepage location query to ORDER BY RAND() LIMIT 3
-    $q = $conn->prepare("SELECT l.*, COUNT(DISTINCT i.id) AS ideas, GROUP_CONCAT(DISTINCT f.feature SEPARATOR '[-]') AS features FROM locations l LEFT JOIN ideas i ON i.location_id = l.id LEFT JOIN location_features f ON f.location_id = l.id GROUP BY l.id LIMIT 3");
+    $q = $conn->prepare("SELECT l.*, COUNT(DISTINCT i.id) AS ideas, GROUP_CONCAT(DISTINCT f.feature SEPARATOR '[-]') AS features FROM locations l LEFT JOIN ideas i ON i.location_id = l.id LEFT JOIN location_features f ON f.location_id = l.id WHERE l.id < 37 OR l.id = 3 GROUP BY l.id ORDER BY ideas DESC, RAND() LIMIT 4");
     $q->execute();
 
     $data = $q->get_result();
@@ -27,7 +29,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBzAMBl8WEWkqExNw16kEk40gCOonhMUmw&callback=initMap" async defer></script>
+        <title>WWYDH | <?php echo isset($_GET["contact"]) ? "Contact" : "Home" ?></title>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,600i,700" rel="stylesheet">
+        <link href="../helpers/header_footer.css" type="text/css" rel="stylesheet" />
+        <link href="styles.css" type="text/css" rel="stylesheet" />
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBzAMBl8WEWkqExNw16kEk40gCOonhMUmw" async defer></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
         <script type="text/javascript">
             // convert location data from php to javascript using JSON
@@ -36,9 +42,10 @@
             function initMap() {
                 // Create a map object and specify the DOM element for display.
                 var map = new google.maps.Map(document.getElementById('map'), {
-                  center: {lat: parseFloat(locations[0].latitude), lng: parseFloat(locations[0].longitude)},
-                  scrollwheel: false,
-                  zoom: 14
+                    animation: google.maps.Animation.DROP,
+                    center: {lat: parseFloat(locations[0].latitude), lng: parseFloat(locations[0].longitude)},
+                    scrollwheel: false,
+                    zoom: 14
                 });
 
                 $(locations).each(function() {
@@ -56,130 +63,173 @@
 
         </script>
 
+        <!-- scroll on click to how it works -->
+        <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                $("#see-how").click(function() {
+                    $("html, body").animate({scrollTop: $("#how").offset().top}, 650);
+                })
+            });
+          </script>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $("li.tablink").click(function() {
+                    if (!$(this).hasClass("active")) {
+                        // handle nav change
+                        $("li.tablink").removeClass("active");
+                        $(this).addClass("active");
+
+                        // handle content change
+                        $(".tabcontent").removeClass("active");
+                        $(".tabcontent[data-tab=" + $(this).data("target") + "]").addClass("active");
+                    }
+                })
+            })
+       </script>
+
+       <script>
+       function openNav() {
+           document.getElementById("mySidenav").style.width = "250px";
+       }
+
+       function closeNav() {
+           document.getElementById("mySidenav").style.width = "0";
+       }
+       </script>
+
+    </head>
+    <body onload="initMap(); openNav();">
+
         <?php
             // FRONTEND: remove this garbage style tag and externalize this stylesheet. This is just so I could see what I was doing
         ?>
-        <style type="text/css">
-            .location_image, .project_image {
-                height: 100px;
-                width: 100px;
-                background-position: center;
-                background-size: cover;
-                float: left;
-            }
-
-            .location {
-                width: 100%;
-                clear: both;
-                margin-bottom: 10px;
-                padding-bottom: 10px;
-                border-bottom: 1px solid rgba(0, 0, 0, 0.05);
-                overflow: hidden;
-            }
-
-            .location:last-child {
-                border-bottom-width: 0px;
-            }
-             #mapContainer{
-              height:500px;
-              position:relative;
-            }
-            #welcome{
-            height: 75px;
-            background-color: #418040;
-            z-index: 1;
-            opacity: 0.9;
-            position: absolute;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            }
-            h1{
-            color: white;
-            font-family: "Open Sans";
-            font-size: large;
-            text-decoration: underline;
-            float: left;
-            vertical-align: middle;
-            position: relative;
-            top: 10px;
-            left: 20px;
-
-            }
-            #locationButton{
-            background-color: white;
-            color: #418040;
-            border: 2px solid #418040;
-            font-family: "Open Sans";
-            float: right;
-            font-size: 22px;
-            border-radius: 8px;
-            padding: 8px 4px;
-            position: relative;
-            top: 13px;
-            left: -30px;
-            }
-            #map{
-            height: 500px;
-            position: relative;
-            }
-        </style>
+        <div id="mySidenav" class="sidenav">
+          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">Close &times;</a>
+          <div id="sideIdea">I Have an Idea</div>
+          <div id="sideLocation">I Have a Location</div>
+          <div id="sideHelp">I Want to Help</div>
+          <div id="sideContact">Contact Us</div>
+        </div>
+        <div id="sideOpen">
+          <span style="font-size:30px;cursor:pointer;color: #939393" onclick="openNav()">&#9776; Menu</span>
+        </div>
     </head>
     <body>
+
         <div id="nav">
-            <div id="logo"></div>
-            <div id="main_nav"></div>
-            <div id="user_nav"></div>
+            <div class="nav-inner width">
+                <a href="../home">
+                    <div id="logo"></div>
+                    <div id="logo_name">What Would You Do Here?</div>
+                <div id="user_nav" class="nav">
+                    <ul>
+                        <a href="#"><li>Log in</li></a>
+                        <a href="#"><li>Sign up</li></a>
+                    </ul>
+                </div>
+                <div id="main_nav" class="nav">
+                    <ul>
+                        <a href="../locations"><li>Locations</li></a>
+                        <a href="../ideas"><li>Ideas</li></a>
+                        <a href="../projects"><li>Projects</li></a>
+                        <a href="../contact"><li>Contact</li></a>
+                    </ul>
+                </div>
+            </div>
         </div>
         <div id="mapContainer">
-          <div id="map"></div>
-          <div id="welcome">
-            <h1>See How it Works!</h1>
-            <div id="locationButton">Submit Location</div>
-          </div>
+            <div id="map">
+            </div>
+            <div id="welcome">
+                <div class="width">
+                    <span id="see-how"><h1>See How it Works!</h1></span>
+                    <div id="locationButton">Submit Location</div>
+                </div>
+            </div>
         </div>
         <div id="explore">
-            <div id="locations">
-                <?php
-                foreach($locations as $l) { ?>
-                    <div class="location">
-                        <?php if ($l["ideas"] > 0) { ?>
-                            <div class="ideas_count"><?php echo $l["ideas"] ?></div>
-                        <?php } ?>
-                        <div class="location_image" style="background-image: url(../helpers/location_images/<?php if (isset($l['image'])) echo $l['image']; else echo "pin.png";?>);"></div>
-                        <div class="address"><?php echo $l["mailing_address"] ?></div>
-                        <?php if (isset($l["features"])) { ?>
-                            <div class="features">
-                                <span>Features:</span>
-                                    <ul>
-                                        <?php foreach ($l["features"] as $f) { ?>
-                                            <li><?php echo $f ?></li>
-                                        <?php } ?>
-                                    </ul>
+            <div class="width">
+                <h1> EXPLORE </h1>
+                <ul class="tab">
+                    <li class="active tablink" data-target="1">Locations</li>
+                    <li class="tablink" data-target="2">Projects</li>
+                </ul>
+                <div id="locations" class="tabcontent active" data-tab="1">
+                    <?php
+                    foreach($locations as $l) { ?>
+                        <div class="location">
+                            <div class="btn-group">
+                                <div class="btn newidea"><a href="../newidea?location=<?php echo $l["id"] ?>">I have an idea</a></div>
+                                <?php if ($l["ideas"] > 0) { ?> <div class="btn seeideas"><a href="../ideas?location=<?php echo $l["id"] ?>">See other ideas here</a></div> <?php } ?>
+                                <div class="btn seelocation"><a href="../locations/propertyInfo.php?id=<?php echo $l["id"] ?>">View full location</a></div>
                             </div>
-                        <?php } ?>
-                        <div class="btn"><a href="../newidea?location=<?php echo $l["id"] ?>">I have an idea</a></div>
-                        <?php if ($l["ideas"] > 0) { ?> <div class="btn"><a href="../ideas?location=<?php echo $l["id"] ?>">See other ideas here</a></div> <?php } ?>
-                        <div class="btn"><a href="../locattion?id=<?php echo $l["id"] ?>">View full location</a></div>
-                    </div>
-                <?php }
-                ?>
-            </div>
-            <div id="projects">
-                <?php
-                foreach ($projects as $p) { ?>
-                    <div class="project">
-                        <div class="project_image" style="background-image: url(../helpers/location_images/<?php if (isset($p['image'])) echo $p['image']; else echo "pin.png";?>);"></div>
-                        <div class="project_leader"><?php echo $p["leader"] ?></div>
-                        <div class="address"><?php echo $p["address"] ?></div>
-                        <div class="project_status">Status: <?php echo $p["completed"] == 0 ? "unfinished" : "finished" ?></div>
-                    </div>
-                <?php } ?>
+                            <div class="location_image" style="background-image: url(../helpers/location_images/<?php if (isset($l['image'])) echo $l['image']; else echo "no_image.jpg";?>);">
+                                <?php if ($l["ideas"] > 0) { ?>
+                                    <div class="ideas_count"><?php echo $l["ideas"] ?></div>
+                                <?php } ?>
+                            </div>
+                            <div class="location_desc">
+                                <div class="address"><?php echo $l["mailing_address"] ?></div>
+                                <?php if (isset($l["features"])) { ?>
+                                    <div class="features">
+                                        <span>Features:</span>
+                                            <ul>
+                                                <?php foreach ($l["features"] as $f) { ?>
+                                                    <li><?php echo $f ?></li>
+                                                <?php } ?>
+                                            </ul>
+                                    </div>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    <?php }
+                    ?>
+                </div>
+                <div id="projects" class="tabcontent" data-tab="2">
+                    <?php
+                    foreach ($projects as $p) { ?>
+                        <div class="project">
+                            <div class="project_image" style="background-image: url(../helpers/location_images/<?php if (isset($p['image'])) echo $p['image']; else echo "no_image.png";?>);"></div>
+                            <div class="project_leader"><?php echo $p["leader"] ?></div>
+                            <div class="address"><?php echo $p["address"] ?></div>
+                            <div class="project_status">Status: <?php echo $p["completed"] == 0 ? "unfinished" : "finished" ?></div>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
         </div>
-        <div id="about"></div>
-        <div id="how"></div>
-        <div id="contact"></div>
-        <div id="footer"></div>
+        <div id="about">
+            <div class="grid-inner width">
+                <h1>ABOUT</h1>
+                <div class="small-content">
+                    WWYDH facilitates economic and social revitalization in Baltimore by combining the existing vacant and underutilized built infrastructure with the creative imagination and skills of people throughout the city. We begin by asking the simplest question:
+                </div>
+
+                <p>"What would you do here?"</p>
+            </div>
+        </div>
+        <div id="how">
+            <div class="grid-inner width">
+                <h1 style="text-align:center"><font color="#3a3a3a">HOW IT WORKS</font></h1>
+                <table>
+                	<tr>
+                		<th><img src="../images/idea.png" /></th>
+                		<th><img src="../images/contributors.png" /></th>
+                		<th><img src="../images/implementation.png" /></th>
+                	</tr>
+                	<tr>
+                		<td>Submit an idea for a location to WWYDH. You can choose to anonymously submit the idea for someone else to eventually lead, or lead the idea yourself. </td>
+                		<td>Ideas can be upvoted and downvoted, so the best rise to the top. A checklist of people and resources the idea will need is generated. Users can contribute to the idea by volunteering to fill one or more of the requirements on the list. When the list is complete, the idea can move to the next stage.</td>
+                		<td>The idea becomes a project, and the project is implemented by everyone who pledged to contribute his or her time and skills to turn a vacant location into a useful space for the community.</td>
+                	</tr>
+                </table>
+            </div>
+        </div>
+        <div id="footer">
+            <div class="grid-inner">
+                &copy; Copyright WWYDH <?php echo date("Y") ?>
+            </div>
+        </div>
     </body>
 </html>
